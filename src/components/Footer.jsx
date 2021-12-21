cae37250897d7030dfc77b8e08fe6cf41b144056
Kristian Kerrigan-Burnard <k.burnard@hotmail.com>
2021-12-21 06:03:42 -0500

    Inital commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,135 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Carol's Crafts</Logo>
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+          condimentum, neque vitae facilisis posuere, nulla nulla accumsan orci,
+          quis ullamcorper odio mi sed lectus. Nam eget iaculis ex. Nam vel.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men's Fashion</ListItem>
+          <ListItem>Women's Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight: "10px"}} /> 4-201 Gladstone Ave, London ON, N5Z 3S1
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight: "10px"}} /> 1 519 902 1408
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight: "10px"}} /> contact@carolscrafts.ca
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"></Payment>
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
